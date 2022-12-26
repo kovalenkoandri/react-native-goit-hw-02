0d5f5c751d8fc4a17a85277ee7012218465f7d2d
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-12-27 00:00:25 +0200

    addPhoto, Registration

--- App.js
+++ App.js
@@ -1,24 +1,45 @@
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
 
 export default function App() {
   return (
-    <ImageBackground style={styles.image} source={require('./assets/bg.png')}>
-      <View style={styles.container}></View>
+    <ImageBackground style={styles.bg} source={require('./assets/bg.png')}>
+      <View style={styles.container}>
+        <Image
+          source={require('./assets/addPhoto.png')}
+          style={styles.addPhoto}
+        />
+        <Text style={styles.regTitle}>Регистрация</Text>
+      </View>
     </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  image: {
+  bg: {
     flex: 1,
-    resizeMode: 'center',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   container: {
-    marginTop: 200,
+    alignItems: 'center',
+    marginTop: 263,
     flex: 1,
     width: '100%',
     backgroundColor: '#fff',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    position: 'relative',
+  },
+  addPhoto: {
+    width: 132,
+    height: 120,
+    position: 'absolute',
+    top: -60,
+  },
+  regTitle: {
+    fontFamily: 'Roboto',
+    fontSize: 30,
+    lineHeight: 35,
+    color: '#212121',
+    position: 'absolute',
+    top: 92,
   },
 });
