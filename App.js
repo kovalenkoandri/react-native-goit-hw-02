7b497347aa636e2e3d6bc88de784afcc3b351814
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-12-26 21:28:10 +0200

    initial

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground style={styles.image} source={require('./assets/bg.png')}>
+      <View style={styles.container}></View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  image: {
     flex: 1,
-    backgroundColor: '#fff',
+    resizeMode: 'center',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  container: {
+    marginTop: 200,
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#fff',
   },
 });
