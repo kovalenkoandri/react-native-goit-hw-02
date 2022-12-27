7577f986aa8f4d91da50ae716d88a44fb76b88df
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-12-27 06:21:29 +0200

    3 TextInput

--- App.js
+++ App.js
@@ -9,8 +9,12 @@ import {
 import React, { useState } from 'react';
 
 export default function App() {
-  const [value, setValue] = useState('');
-  const inputHandler = (text) => setValue(text);
+  const [loginValue, setLoginValue] = useState('');
+  const [emailValue, setEmailValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+  const inputLoginHandler = (text) => setLoginValue(text);
+  const inputEmailHandler = (text) => setEmailValue(text);
+  const inputPasswordHandler = (text) => setPasswordValue(text);
   return (
     <ImageBackground style={styles.bg} source={require('./assets/bg.png')}>
       <View style={styles.container}>
@@ -20,10 +24,23 @@ export default function App() {
         />
         <Text style={styles.regTitle}>Регистрация</Text>
         <TextInput
-          placeholder="Type text"
-          value={value}
-          onChangeText={inputHandler}
+          placeholder="Логин"
+          value={loginValue}
+          onChangeText={inputLoginHandler}
           style={styles.inputLogin}
+        />
+        <TextInput
+          placeholder="Адрес электронной почты"
+          value={emailValue}
+          onChangeText={inputEmailHandler}
+          style={styles.inputEmail}
+        />
+        <TextInput
+          placeholder="Пароль"
+          value={passwordValue}
+          onChangeText={inputPasswordHandler}
+          style={styles.inputPassword}
+          secureTextEntry={true}
         />
       </View>
     </ImageBackground>
@@ -67,6 +84,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#F6F6F6',
     borderWidth: 1,
     borderColor: '#E8E8E8',
+    paddingLeft: 16,
+  },
+  inputEmail: {
+    top: 226,
+    position: 'absolute',
+    width: 343,
+    height: 50,
+    borderRadius: 8,
+    backgroundColor: '#F6F6F6',
+    borderWidth: 1,
+    borderColor: '#E8E8E8',
     borderStyle: 'solid',
+    paddingLeft: 16,
+  },
+  inputPassword: {
+    top: 292,
+    position: 'absolute',
+    width: 343,
+    height: 50,
+    borderRadius: 8,
+    backgroundColor: '#F6F6F6',
+    borderWidth: 1,
+    borderColor: '#E8E8E8',
+    borderStyle: 'solid',
+    paddingLeft: 16,
+    paddingRight: 16,
   },
 });
