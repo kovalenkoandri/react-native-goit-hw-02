18e0271842bdf4675172041cfeda0156daf29e3c
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-01-26 04:28:23 +0200

    () => setHidePass(!hidePass)

---
+++ Screens/LoginScreen.js
@@ -0,0 +1,71 @@
+import {
+  Text,
+  View,
+  ImageBackground,
+  Image,
+  TextInput,
+  Pressable,
+} from 'react-native';
+import React, { useState } from 'react';
+import { styles } from '../styles';
+
+export function LoginScreen() {
+  const [loginValue, setLoginValue] = useState('');
+  const [emailValue, setEmailValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+  const [hidePass, setHidePass] = useState(true);
+  const inputLoginHandler = (text) => setLoginValue(text);
+  const inputEmailHandler = (text) => setEmailValue(text);
+  const inputPasswordHandler = (text) => setPasswordValue(text);
+  const onSubmitHandle = (creds) => console.log({ ...creds });
+  return (
+    <ImageBackground style={styles.bg} source={require('../assets/bg.png')}>
+      <View style={styles.container}>
+        <Image
+          source={require('../assets/addPhoto.png')}
+          style={styles.addPhoto}
+        />
+        <View style={styles.regTitleWrapper}>
+          <Text style={styles.regTitle}>Регистрация</Text>
+        </View>
+        <TextInput
+          placeholder="Логин"
+          value={loginValue}
+          onChangeText={inputLoginHandler}
+          style={styles.inputLogin}
+        />
+        <TextInput
+          placeholder="Адрес электронной почты"
+          value={emailValue}
+          onChangeText={inputEmailHandler}
+          style={styles.inputEmail}
+        />
+        <View style={styles.inputPasswordWrapper}>
+          <TextInput
+            placeholder="Пароль"
+            value={passwordValue}
+            onChangeText={inputPasswordHandler}
+            style={styles.inputPassword}
+            secureTextEntry={hidePass ? true : false}
+          />
+          <Pressable
+            style={styles.buttonShow}
+            onPress={() => setHidePass(!hidePass)}
+          >
+            <Text style={styles.textShow}>
+              {hidePass ? 'Скрыть' : 'Показать'}
+            </Text>
+          </Pressable>
+        </View>
+        <Pressable
+          style={styles.buttonRegister}
+          onPress={() =>
+            onSubmitHandle({ loginValue, emailValue, passwordValue })
+          }
+        >
+          <Text style={styles.textRegister}>Зарегистрироваться</Text>
+        </Pressable>
+      </View>
+    </ImageBackground>
+  );
+}
---
+++ Screens/RegistrationScreen.js
@@ -0,0 +1,59 @@
+import {
+  Text,
+  View,
+  ImageBackground,
+  Image,
+  TextInput,
+  Pressable,
+} from 'react-native';
+import React, { useState } from 'react';
+import { styles } from '../styles';
+
+export function LoginScreen() {
+  const [loginValue, setLoginValue] = useState('');
+  const [emailValue, setEmailValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+  const inputLoginHandler = (text) => setLoginValue(text);
+  const inputEmailHandler = (text) => setEmailValue(text);
+  const inputPasswordHandler = (text) => setPasswordValue(text);
+  return (
+    <ImageBackground style={styles.bg} source={require('../assets/bg.png')}>
+      <View style={styles.container}>
+        <Image
+          source={require('../assets/addPhoto.png')}
+          style={styles.addPhoto}
+        />
+        <View style={styles.regTitleWrapper}>
+          <Text style={styles.regTitle}>Регистрация</Text>
+        </View>
+        <TextInput
+          placeholder="Логин"
+          value={loginValue}
+          onChangeText={inputLoginHandler}
+          style={styles.inputLogin}
+        />
+        <TextInput
+          placeholder="Адрес электронной почты"
+          value={emailValue}
+          onChangeText={inputEmailHandler}
+          style={styles.inputEmail}
+        />
+        <View style={styles.inputPasswordWrapper}>
+          <TextInput
+            placeholder="Пароль"
+            value={passwordValue}
+            onChangeText={inputPasswordHandler}
+            style={styles.inputPassword}
+            secureTextEntry={true}
+          />
+          <Pressable style={styles.buttonShow} onPress>
+            <Text style={styles.textShow}>Показать</Text>
+          </Pressable>
+        </View>
+        <Pressable style={styles.buttonRegister} onPress>
+          <Text style={styles.textRegister}>Зарегистрироваться</Text>
+        </Pressable>
+      </View>
+    </ImageBackground>
+  );
+}
